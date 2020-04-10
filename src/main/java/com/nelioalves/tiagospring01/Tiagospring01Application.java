92838262101d66e74ed331172881fc30198d1e90
Tiago Nascimento <tiagonascimento.pfloyd@gmail.com>
2020-04-10 17:52:17 -0300

    projeto criado

---
+++ src/main/java/com/nelioalves/tiagospring01/Tiagospring01Application.java
@@ -0,0 +1,13 @@
+package com.nelioalves.tiagospring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiagospring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tiagospring01Application.class, args);
+	}
+
+}
