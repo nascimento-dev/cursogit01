82eb72bd4d70e4705e514c33538e644e4a2e6fc6
Tiago Nascimento <tiagonascimento.pfloyd@gmail.com>
2020-04-17 19:22:07 -0300

    Ajustes no endpoint / Categorias / {id}

--- src/main/java/com/nelioalves/tiagospring01/services/CategoriaService.java
+++ src/main/java/com/nelioalves/tiagospring01/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nelioalves.tiagospring01.domain.Categoria;
 import com.nelioalves.tiagospring01.repositories.CategoriaRepository;
+import com.nelioalves.tiagospring01.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 		
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
 
